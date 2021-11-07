fc7c2c6a23671288ec49ea21fe6ed157fff07716
Samuel Saraiva Campos <samuka125@gmail.com>
2021-11-07 14:07:36 -0300

    Parte 1 do BackEnd

---
+++ BackEnd/todo_app/apis/urls.py
@@ -0,0 +1,9 @@
+from typing import List
+from django.urls import path
+
+from .views import ListTodo, DetailTodo
+
+urlpatterns = [
+    path('', ListTodo.as_view()),
+    path('<int:pk>/', DetailTodo.as_view())
+]
